6e72a9df9809fc5e99e489ba008c0124ecf4a2ea
hackermat <villacresa@gmail.com>
2023-07-07 19:34:32 -0500

    loginInicio

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,16 +1,15 @@
-import { Route, Routes } from "react-router";
-import Landing from "./pages/Landing";
-import Login from "./pages/Login";
-import { Dashboard } from "./pages/Dashboard";
+import { Route, Routes } from 'react-router'
+import Landing from './pages/Landing'
+import Login from './pages/Login'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
-  );
+      <Route path= '/' element={<Landing />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>  
+    
+  )
 }
 
 export default App;
