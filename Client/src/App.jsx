a0835ed7bdfd8cae82831ec0b05898d88257a2f9
jcdev92 <jcdeveloper92@gmail.com>
2023-07-11 14:20:33 -0400

    maquetando el kardex

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
+import { Stock } from './pages/Stock'
 
 function App() {
   return (
     <Routes>
       <Route path= '/' element={<Landing />} />
       <Route path='/login' element={<Login />} />
+      <Route path='/stock' element={<Stock />} />
     </Routes>  
     
   )
