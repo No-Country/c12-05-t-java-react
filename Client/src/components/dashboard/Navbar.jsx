1bf33bf3fe710f37984804807622a26a61810ee4
jcdev92 <jcdeveloper92@gmail.com>
2023-07-12 00:01:45 -0400

    maquetando el dashboard

--- Client/src/components/dashboard/Navbar.jsx
+++ Client/src/components/dashboard/Navbar.jsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
         <div className="w-1/4 flex justify-center items-center gap-5">
           <img
             className="rounded-full"
-            src="../../logoHammer.png"
+            src="../../public/logoHammer.png"
             height="30px"
             width="30px"
           />
