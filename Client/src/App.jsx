59f451c43722e94e5c768adeeed31acad8ae9f3d
jcdev92 <jcdeveloper92@gmail.com>
2023-07-12 14:48:51 -0400

    rama jesus merge

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,10 @@
-
 import { Route, Routes } from 'react-router'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
-
+import { Dashboard } from './pages/Dashboard'
 
 function App() {
   return (
-
     <Routes>
       <Route path= '/' element={<Landing />} />
       <Route path='/login' element={<Login />} />
---
+++ Client/src/components/dashboard/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoMdContact } from "react-icons/io";
+import { IoMdContacts } from "react-icons/io";
+import { PiListChecksBold } from "react-icons/pi";
+
+export const Navbar = () => {
+  return (
+    <div className="col-span-4 border-2 border-gray-400">
+      <div className="flex justify-between h-full items-center">
+        <div className="w-1/4 flex justify-center items-center gap-5">
+          <img
+            className="rounded-full"
+            src="../../logoHammer.png"
+            height="30px"
+            width="30px"
+          />
+          <spam className="text-2xl font-saira">
+            <h1>STOCK WISE</h1>
+          </spam>
+        </div>
+        <div className="w-2/6 flex gap-2 p-2">
+          <ul className="flex w-full justify-around">
+            <li>
+              <Link to="/users" className="flex gap-2">
+                <IoMdContacts className="text-2xl" />
+                <span className="">usuarios</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/dashboard" className="flex gap-2">
+                <PiListChecksBold className="text-2xl" />
+                <span className="">inventario</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/profile">
+                <IoMdContact className="text-2xl" />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
