4152ed81fc7797183c20bd91f7727d47fd3eb668
hackermat <villacresa@gmail.com>
2023-07-12 20:35:51 -0500

    login

--- Client/src/pages/Landing.jsx
+++ Client/src/pages/Landing.jsx
@@ -1,27 +1,6 @@
 import ImgLanding from '../assets/img/ImgLanding.png'
-import { Link } from 'react-router-dom'
+import { useAuth0 } from "@auth0/auth0-react";
 import logo from '../assets/img/logo.png'
-import ImgLanding from '../assets/img/ImgLanding.png'
-
-const Landing = () => {
-  const  { loginWithPopup, user } = useAuth0();
-  const navigate = useNavigate();
-
-  const handleLogin = async () => {
-    await loginWithPopup();
-  };
-
-  useEffect(() => {
-    try {
-      if (user) {
-        navigate('/dashboard')
-        console.log("Exito")
-      }
-    } catch (error) {
-      console.log(error);
-    }
-
-  }, [user]);
 
 
 const Landing = () => {
@@ -49,11 +28,11 @@ const Landing = () => {
             </div>
             <h2 className='text-[#FFFFFF] text-6xl font-semibold text-center tracking-wide mt-20' >Bienvenido!</h2>
             <div className='flex justify-center'>
-              <Link to='/login'>
-              <button className='bg-[#B89754] hover:bg-[#826326] transition-all text-white text-xl font-medium p-4 px-14 rounded-md shadow-sm shadow-black m-auto mt-32'>
+
+              <button className='bg-[#B89754] hover:bg-[#826326] transition-all text-white text-xl font-medium p-4 px-14 rounded-md shadow-sm shadow-black m-auto mt-32' onClick={handleLogin}>
                 Ingresar
               </button>
-              </Link>
+            
             </div>
           </div> 
       </div>
