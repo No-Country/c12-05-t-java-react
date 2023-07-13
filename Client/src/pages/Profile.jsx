ccfe4f4dc8166641de209d3581c8297bbe19c505
hackermat <villacresa@gmail.com>
2023-07-12 20:35:51 -0500

    login

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -1,34 +1,18 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import LogoutButton from "./Logout";
 
-const Profile = () => {
+export const Profile = () => {
   const { user, isAuthenticated, isloading } = useAuth0();
 
-  if (isloading) {
-    return <div> Loading...</div>;
+  if(isloading){
+    return <div> Loading...</div>
   }
-  return (
-    isAuthenticated && (
-      <div className="flex h-full flex-col items-center justify-center gap-4">
-        <div className="flex flex-col items-center justify-center rounded-lg bg-transparent shadow-2xl w-3/12">
-          <div className="w-full">
-            <img
-              className="bg-cover bg-center w-full rounded-t-md"
-              src={user.picture}
-              alt={user.name}
-            />
-          </div>
-          <div className="flex flex-col gap-4 p-6">
-            <h2>{user.name}</h2>
-            <p> Email: {user.email}</p>
-          </div>
-        </div>
-        <div className="">
-          <LogoutButton />
-        </div>
-      </div>
-    )
-  );
-};
-
-export default Profile;
+  return(
+    
+    isAuthenticated &&
+    <div>
+      <img src={user.picture} alt={user.name} />
+      <h2>{user.name}</h2>
+      <p> Email: {user.email}</p>
+    </div>
+  )
+}
