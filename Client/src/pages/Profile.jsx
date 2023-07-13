fd2dfd10dbc99543ea1d2c814720bde98f4c8f78
hackermat <villacresa@gmail.com>
2023-07-12 21:51:43 -0500

    logini3

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
-export const Profile = () => {
+const Profile = () => {
   const { user, isAuthenticated, isloading } = useAuth0();
 
   if(isloading){
@@ -16,3 +16,5 @@ export const Profile = () => {
     </div>
   )
 }
+
+export default Profile;
