d5853d5f9cb39bda5d8abb027947c87e54e5e868
jcdev92 <jcdeveloper92@gmail.com>
2023-07-13 13:09:51 -0400

    maquetando el inventario

--- Client/src/components/dashboard/Stock.jsx
+++ Client/src/components/dashboard/Stock.jsx
@@ -4,66 +4,81 @@ export const Stock = () => {
   return (
     <div className="border-2 border-gray-400 col-span-3">
       <div className="flex justify-center py-8">
-        <div class="relative overflow-auto shadow-md sm:rounded-lg">
-          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" class="px-6 py-3">
+        <div className="relative overflow-auto shadow-md sm:rounded-lg w-5/6">
+          <table className="w-full text-xs text-left dark:text-gray-400">
+            <thead className="text-xs uppercase bg-transparent dark:text-gray-400">
+              <tr className="text-neutral-900">
+                <th scope="col" className="px-6 py-3">
                   codigo
                 </th>
-                <th scope="col" class="px-6 py-3">
+                <th scope="col" className="px-6 py-3">
                   nombre
                 </th>
-                <th scope="col" class="px-6 py-3">
+                <th scope="col" className="px-6 py-3">
                   descripcion
                 </th>
-                <th scope="col" class="px-6 py-3">
+                <th scope="col" className="px-6 py-3">
                   cantidad
                 </th>
-                <th scope="col" class="px-6 py-3">
-                  <span class="sr-only">Edit</span>
+                <th scope="col" className="px-6 py-3">
+                  <span className="sr-only">Edit</span>
                 </th>
               </tr>
             </thead>
             <tbody>
-              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="px-6 py-4">Silver</td>
-                <td class="px-6 py-4 ">Apple MacBook Pro 17"</td>
-                <td class="px-6 py-4">Laptop</td>
-                <td class="px-6 py-4">$2999</td>
-                <td class="px-6 py-4 text-right">
-                  <a
-                    href="#"
-                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
+              <tr className="bg-transparent border-2 border-neutral-300 text-neutral-900 hover:bg-neutral-300 ">
+                <td scope="row" className="px-6 py-4">
+                  XNSDJ1046M
+                </td>
+                <td scope="row" className="px-6 py-4 ">
+                  Kit Taladro"
+                </td>
+                <td scope="row" className="px-6 py-4">
+                  Taladro Rotomartillo Percutor Inalambrico 1/2" 2*2.0ah 69pz
+                </td>
+                <td scope="row" className="px-6 py-4 text-center">
+                  13
+                </td>
+                <td scope="row" className="px-6 py-4 text-right">
+                  <a href="#" className="font-medium hover:text-neutral-500">
                     Edit
                   </a>
                 </td>
               </tr>
-              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="px-6 py-4">White</td>
-                <td class="px-6 py-4">Microsoft Surface Pro</td>
-                <td class="px-6 py-4">Laptop PC</td>
-                <td class="px-6 py-4">$1999</td>
-                <td class="px-6 py-4 text-right">
-                  <a
-                    href="#"
-                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
+              <tr className="bg-transparent border-2 border-neutral-300 text-neutral-900 hover:bg-neutral-300 ">
+                <td scope="row" className="px-6 py-4">
+                  ZADFIVMRT0
+                </td>
+                <td scope="row" className="px-6 py-4">
+                  Desarmador punta
+                </td>
+                <td scope="row" className="px-6 py-4">
+                  Desarmador punta intercambiable
+                </td>
+                <td scope="row" className="px-6 py-4 text-center">
+                  20
+                </td>
+                <td scope="row" className="px-6 py-4 text-right">
+                  <a href="#" className="font-medium hover:text-neutral-500">
                     Edit
                   </a>
                 </td>
               </tr>
-              <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="px-6 py-4">Black</td>
-                <td class="px-6 py-4 ">Magic Mouse 2</td>
-                <td class="px-6 py-4">Accessories</td>
-                <td class="px-6 py-4">$99</td>
-                <td class="px-6 py-4 text-right">
-                  <a
-                    href="#"
-                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
+              <tr className="bg-transparent border-2 border-neutral-300 text-neutral-900 hover:bg-neutral-300 ">
+                <td scope="row" className="px-6 py-4">
+                  TRSXY56H8L
+                </td>
+                <td scope="row" className="px-6 py-4 ">
+                  Tornillo 1/2"
+                </td>
+                <td scope="row" className="px-6 py-4">
+                  Pack 6 tornillos de 1/2"
+                </td>
+                <td scope="row" className="px-6 py-4 text-center">
+                  40
+                </td>
+                <td scope="row" className="px-6 py-4 text-right">
+                  <a href="#" className="font-medium hover:text-neutral-500">
                     Edit
                   </a>
                 </td>
