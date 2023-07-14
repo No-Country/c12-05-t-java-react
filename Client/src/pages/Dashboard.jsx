04b2e7f2085f25e79012327d3eec7f77279f0464
jcdev92 <jcdeveloper92@gmail.com>
2023-07-13 20:10:18 -0400

    anadido el svg de las herramientas

--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -8,7 +8,15 @@ export const Dashboard = () => {
     <div className="grid grid-cols-4 grid-rows-[0.2fr,0.3fr,2fr] gap-1 min-h-screen border-2 border-gray-400 bg-neutral-200">
       <Navbar />
       <SearchBar />
-      <div className="border-2 border-gray-400 row-span-2"></div>
+      <div className="border-2 border-gray-400 row-span-2">
+        <div className="flex justify-end items-end h-full">
+          <img
+            className="h-4/6 w-4/6"
+            src="../../twemoji_hammer-and-wrench.svg"
+            alt="hammer and wrench"
+          />
+        </div>
+      </div>
       <Stock />
     </div>
   );
