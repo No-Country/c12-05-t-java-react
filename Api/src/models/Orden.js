5ac4da487cdfd3d84507b8d2e79cb6afe2bbea54
hackermat <villacresa@gmail.com>
2023-07-26 11:34:23 -0500

    backend

---
+++ Api/src/models/Orden.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define('orden', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      allowNull: false,
+      primaryKey: true
+    },
+ 
+    createdate: {
+      type: DataTypes.DATEONLY, 
+      allowNull: true,
+    },
+    status: {
+      type: DataTypes.ENUM('approve', 'cancel', 'delibery', 'traveling'),
+      allowNull: true
+    },
+    shippingaddress: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    shippingpromise: {
+      type: DataTypes.DATEONLY
+    }
+  }, { timestamps: false });
+};
